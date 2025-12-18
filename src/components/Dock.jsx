52099e9e5c6f71bcd669ad17634bd584fb433026
Navaneeth Gowda T <navaneeth@datatobiz.com>
2025-12-18 19:49:38 +0530

    made window.js and applied window functionality to dock.jsx

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -4,10 +4,11 @@ import { useRef } from 'react';
 import { Tooltip } from 'react-tooltip';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import useWindowStore from '#store/window.js';
 
 const Dock = () => {
   const dockRef = useRef(null);
-  const toggleApp = (app) => {};
+  const { openWindow, closeWindow, windows } = useWindowStore();
   
   useGSAP(() => {
     const dock = dockRef.current;
@@ -54,7 +55,25 @@ const Dock = () => {
     };
   }, []);
 
-  
+  const toggleApp = (app) => {
+    if(!app.canOpen) return;
+
+    const window = windows[app.id];
+
+    if(!window.isOpen){
+        closeWindow(app.id);
+    }else{
+        openWindow(app.id);
+    }
+
+    if (window.isOpen){
+        closeWindow(app.id);
+    }
+    else {
+        openWindow(app.id);
+    }
+    console.log(windows);
+  };
 
   return (
     <section id = "dock">
