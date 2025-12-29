9019761c07091fc3a539bd6d7c5776025f8bfe35
Navaneeth Gowda T <gnavaneetht@gmail.com>
2025-12-29 16:04:02 +0530

    Finished Finder window

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -5,11 +5,25 @@ import WindowWrapper from '#hoc/WindowWrapper';
 import { locations } from '#constant';
 import useLocationStore from '#store/location';
 import clsx from 'clsx';
+import useWindowStore from '#store/window';
 
 const Finder = () => {
+    const { openWindow } = useWindowStore();
     const { activeLocation, setActiveLocation } = useLocationStore();
 
-    const renderList = (items) => items.map((item) => (
+    const openItem = (item) => {
+        if(item.fileType === "pdf") return openWindow('resume');
+        if(item.kind ==="folder") return setActiveLocation(item);
+        if(['fig', 'url'].includes(item.fileType) && item.href) return window.open(item.href, '_blank');
+
+        openWindow(`${item.fileType}${item.kind}`, item);
+    };
+
+    const renderList = (name ,items) => (
+        <div>
+            <h3>{name}</h3>
+        <ul>
+        {items.map((item) => (
         <li key = {item.id} 
         onClick = {() =>
             setActiveLocation(item)} 
@@ -18,7 +32,10 @@ const Finder = () => {
             )} >
             <img src={item.icon} className='w-4' alt = {item.name} />
             <p className='text-sm font-medium truncate'>{item.name}</p>    
-        </li>))
+        </li>))}
+        </ul>
+        </div>
+        );
 
   return (
     <>
@@ -29,45 +46,18 @@ const Finder = () => {
 
     <div className='bg-white flex h-full'>
         <div className='sidebar'>
-            <div>
-                <h3>Favorites</h3>
-                <ul>
-                    {
-                        Object.values(locations).map((item) => (
-                            <li key = {item.id} 
-                            onClick = {() =>
-                                setActiveLocation(item)} 
-                                className = {clsx(
-                                    item.id ===activeLocation.id ? "active" : "not-active",
-                                )} >
-                                <img src={item.icon} className='w-4' alt = {item.name} />
-                                <p className='text-sm font-medium truncate'>{item.name}</p>    
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
-
-            <div>
-                <h3>Work</h3>
-                <ul>
-                    {
-                        locations.work.children.map((item) => (
-                            <li key = {item.id} 
-                            onClick = {() =>
-                                setActiveLocation(item)} 
-                                className = {clsx(
-                                    item.id ===activeLocation.id ? "active" : "not-active",
-                                )} >
-                                <img src={item.icon} className='w-4' alt = {item.name} />
-                                <p className='text-sm font-medium truncate'>{item.name}</p>    
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
-
+                    {  renderList("Favorites", Object.values(locations))}
+                    {  renderList("Work",locations.work.children)}
         </div>
+
+            <ul className='content'>
+                {activeLocation?.children.map((item) =>(
+                    <li key ={item.id} className={item.position} onClick={() => openItem(item)}>
+                        <img src={item.icon} alt="{item.name}" />
+                        <p>{item.name}</p>
+                    </li>
+                ))}
+            </ul>
     </div>      
     </>
   )
